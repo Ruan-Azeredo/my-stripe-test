3adc3a0643f70a4be6d00c2020a1ce9ba10155ab
Ruan Ubunto <ruanazeredo@gmail.com>
2025-06-07 22:57:07 -0300

    tada: init

---
+++ apps/server/server.js
@@ -0,0 +1,11 @@
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    console.log(`Received request for: ${req.url}`)
+    res.end('Hello, World!\n')
+})
+
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000/')
+})
