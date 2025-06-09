14142797f86609a9f06aec361f123d5e3e820f37
Ruan Ubunto <ruanazeredo@gmail.com>
2025-06-08 23:36:03 -0300

    :sparkles: feat: pefilled email

--- apps/stripe-test-web/src/App.tsx
+++ apps/stripe-test-web/src/App.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 const VITE_REACT_APP_PAYMENT_LINK = import.meta.env.VITE_REACT_APP_PAYMENT_LINK;
 
 function App() {
 
+  const [email, setEmail] = useState<string>('');
+
   const handleClick = () => {
     if (VITE_REACT_APP_PAYMENT_LINK) {
-      window.location.href = VITE_REACT_APP_PAYMENT_LINK;
+      window.location.href = VITE_REACT_APP_PAYMENT_LINK + '?prefilled_email=' + email
     }
   }
 
   return (
     <>
+      <input type="email" onChange={(e) => setEmail(e.target.value)}/>
       <button onClick={handleClick}>Stripe</button>
     </>
   )
